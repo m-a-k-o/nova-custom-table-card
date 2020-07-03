0ecb106488f0a605a5cb7dee57d165dcefb7542e
Dániel Zoltán Tolnai <daniel.tolnai@apicore.de>
2020-07-03 14:55:57 +0200

    Hide empty table header

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -3,7 +3,7 @@
         <card class="flex flex-col">
           <h1 v-if="title" class="py-6 text-90 font-normal text-2xl text-left pl-4">{{ title }}</h1>
           <table cellpadding="0" cellspacing="0" data-testid="resource-table" class="table w-full">
-            <thead>
+            <thead v-if="header && header.length > 0">
               <tr>
                 <th :class="head.class" :id="head.id" v-for="head in header">
                   <span class="cursor-pointer inline-flex items-center">
